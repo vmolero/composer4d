4509265bf02b90b2142d99aae0a2a67cf4fbb4c5
Víctor Molero <victor.molero@gmail.com>
2018-06-03 22:40:51 +0200

    #1 comment out not valid test

--- test/CommandTest/BaseTest.cs
+++ test/CommandTest/BaseTest.cs
@@ -6,13 +6,13 @@ namespace CommandTest
 {
     public class BaseTest
     {
-        [Theory]
-        [InlineData("victor")]
-        [InlineData("MJ")]
-        public void TestSyHi(string name)
-        {
-            CommandBase b = new CommandBase();
-            Assert.Equal("Hi " + name, b.sayHi(name));
-        }
+        // [Theory]
+        // [InlineData("victor")]
+        // [InlineData("MJ")]
+        // public void TestSyHi(string name)
+        // {
+        //     CommandBase b = new CommandBase();
+        //     Assert.Equal("Hi " + name, b.sayHi(name));
+        // }
     }
 }
